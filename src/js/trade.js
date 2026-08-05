e811beacd53ad0c4311184305a5e2ee2698108e6
Zarys <zarys.exists@gmail.com>
2026-08-05 13:05:42 +0530

    Update trade.js

--- src/js/trade.js
+++ src/js/trade.js
@@ -4,7 +4,7 @@ import "./nav.js";
 
 FTFAuth.init();
 
-const LAST_UPDATED = "01 August";
+const LAST_UPDATED = "05 August";
 const HV_DIVISOR = 30;
 const MAX_SLOTS = 27;
 const MAX_QUANTITY = 100;
